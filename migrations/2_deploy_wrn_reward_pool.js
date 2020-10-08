980a07022e6ac64ce1f35fbb3e7829aab6a312ad
Sebastian Kim <sydneyitguy@gmail.com>
2020-10-08 15:14:10 +0900

    Fix migration

--- migrations/2_deploy_wrn_reward_pool.js
+++ migrations/2_deploy_wrn_reward_pool.js
@@ -3,19 +3,25 @@ const ERC20Token = artifacts.require('ERC20Token');
 const WRNRewardPool = artifacts.require('WRNRewardPool');
 const { toBN } = require('../test/helpers/NumberHelpers');
 
-module.exports = async function (deployer, network, accounts) {
+module.exports = async function (deployer, network, [creator]) {
   if (['development', 'ganache'].indexOf(network) > -1) {
-    await deployer.deploy(ERC20Token, 'Neverlose.money', 'WRN', toBN(10000));
+    await deployer.deploy(ERC20Token, { from: creator });
     const wrnToken = await ERC20Token.deployed();
+    hunt.initialize('Neverlose.money', 'WRN', toBN(10000));
 
-    await deployer.deploy(ERC20Token, 'HUNT Token', 'WRN', toBN(10000));
+    await deployer.deploy(ERC20Token, { from: creator });
     const hunt = await ERC20Token.deployed();
-    await deployer.deploy(ERC20Token, 'Wrapped ETH', 'WETH', toBN(10000));
-    const weth = await ERC20Token.deployed();
-    await deployer.deploy(ERC20Token, 'Wrapped BTC', 'WBTC', toBN(10000));
-    const wbtc = await ERC20Token.deployed();
+    hunt.initialize('HUNT Token', 'WRN', toBN(10000));
 
-    await deployer.deploy(WRNRewardPool);
+    await deployer.deploy(ERC20Token, { from: creator });
+    const weth = await ERC20Token.deployed();
+    weth.initialize('Wrapped ETH', 'WETH', toBN(10000));
+
+    await deployer.deploy(ERC20Token, { from: creator });
+    const wbtc = await ERC20Token.deployed();
+    wbtc.initialize('Wrapped BTC', 'WBTC', toBN(10000));
+
+    await deployer.deploy(WRNRewardPool, { from: creator });
     const wrnRewardPool = await WRNRewardPool.deployed();
     await wrnRewardPool.initialize(wrnToken.address);
 
@@ -27,6 +33,7 @@ module.exports = async function (deployer, network, accounts) {
     console.log(` - HUNT: ${hunt.address}\n - WETH: ${weth.address}\n - WBTC: ${wbtc.address}`);
     console.log(`Owner: ${await wrnRewardPool.owner()} / Dev: ${await wrnRewardPool.devAddress()}`);
     console.log(`Sum of reward pool multiplers: ${await wrnRewardPool.totalMultiplier()}`);
+    console.log(`Owner HUNT balance: ${await hunt.balanceOf(creator)}`);
   } else if (['ropsten'].indexOf(network) > -1) {
     // TODO:
   } else if (network === 'mainnet') {
