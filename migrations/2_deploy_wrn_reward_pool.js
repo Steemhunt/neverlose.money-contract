df1bf4e31558eceeb4a555c90116efbcf784ad8c
Sebastian Kim <sydneyitguy@gmail.com>
2020-10-23 13:54:46 +0900

    Ropsten deploy script

--- migrations/2_deploy_wrn_reward_pool.js
+++ migrations/2_deploy_wrn_reward_pool.js
@@ -5,22 +5,22 @@ module.exports = async function (deployer, network, [creator]) {
   const WRNRewardPool = artifacts.require('WRNRewardPool');
   const { toBN } = require('../test/helpers/NumberHelpers');
 
-  if (['development', 'ganache'].indexOf(network) > -1) {
+  if (['development', 'ganache', 'ropsten'].indexOf(network) > -1) {
     await deployer.deploy(ERC20Token, { from: creator });
     const wrnToken = await ERC20Token.deployed();
-    wrnToken.initialize('Neverlose.money', 'WRN', toBN(10000));
+    wrnToken.initialize('Neverlose.money', 'WRN', toBN(100000));
 
     await deployer.deploy(ERC20Token, { from: creator });
     const hunt = await ERC20Token.deployed();
-    hunt.initialize('HUNT Token', 'HUNT', toBN(10000));
+    hunt.initialize('HUNT Token', 'HUNT', toBN(100000));
 
     await deployer.deploy(ERC20Token, { from: creator });
     const weth = await ERC20Token.deployed();
-    weth.initialize('Wrapped ETH', 'WETH', toBN(10000));
+    weth.initialize('Wrapped ETH', 'WETH', toBN(100000));
 
     await deployer.deploy(ERC20Token, { from: creator });
     const wbtc = await ERC20Token.deployed();
-    wbtc.initialize('Wrapped BTC', 'WBTC', toBN(10000));
+    wbtc.initialize('Wrapped BTC', 'WBTC', toBN(100000));
 
     await deployer.deploy(WRNRewardPool, { from: creator });
     const wrnRewardPool = await WRNRewardPool.deployed();
@@ -37,8 +37,6 @@ module.exports = async function (deployer, network, [creator]) {
     console.log(`Owner: ${await wrnRewardPool.owner()} / Dev: ${await wrnRewardPool.devAddress()}`);
     console.log(`Sum of reward pool multiplers: ${await wrnRewardPool.totalMultiplier()}`);
     console.log(`Owner HUNT balance: ${await hunt.balanceOf(creator)}`);
-  } else if (['ropsten'].indexOf(network) > -1) {
-    // TODO:
   } else if (network === 'mainnet') {
     const huntTokenOnMainnet = '0x9aab071b4129b083b01cb5a0cb513ce7eca26fa5';
     // TODO:
